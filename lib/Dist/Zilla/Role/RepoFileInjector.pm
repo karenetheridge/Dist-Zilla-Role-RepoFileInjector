edd9c8c7daac01ac35860b37a17d4903a1980cf4
Karen Etheridge <ether@cpan.org>
2016-05-31 20:59:12 -0700

    increment $VERSION after 0.007 release

--- lib/Dist/Zilla/Role/RepoFileInjector.pm
+++ lib/Dist/Zilla/Role/RepoFileInjector.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Role::RepoFileInjector;
 # KEYWORDS: plugin distribution generate create file repository
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '0.007';
+our $VERSION = '0.008';
 
 use Moose::Role;
 
